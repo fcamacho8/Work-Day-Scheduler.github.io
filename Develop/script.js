f9527db837c69ade7f4fb09e8c54370a5d1f3c56
FCTESLA <fcamacho888@gmail.com>
2020-03-15 21:58:37 -0700

    ClickEvent

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 var currentDay = moment().format('MMMM Do YYYY');
 var currentTime = moment().format('h:mm a');
-var hours = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+var hours = ["7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5"];
 
 
 $("#currentDay").text(currentDay);
@@ -11,10 +11,10 @@ $("#currentTime").text(currentTime);
 function renderPage() {
     $(".container").empty();
     for (var i = 0; i < hours.length; i++) {
-        var hourBlock = $("<div>").attr("class", "time-block row");
-        var time = $("<div>").attr("class", "hour col-sm-1").text(hours[i] + ":00");
-        var text = $("<textarea>").addClass("text col-sm-10").attr("id", "inputText" + i);
-        var save = $("<button>").addClass("saveBtn col-sm-1").attr("save", hours[i]).addClass("far fa-save");
+        var hourBlock = $("<div>").attr("class", "row time-block");
+        var time = $("<div>").attr("class", "hour col-sm-1 ").text(hours[i] + ":00");
+        var text = $("<textarea>").addClass("col-sm-10").addClass("text").attr("id", "inputText" + i);
+        var save = $("<button>").attr("class", "col-sm-1").addClass("saveBtn").attr("data-input", "inputText" + i).attr("saveTime", hours[i]).addClass("far fa-save");
 
 
 
@@ -29,9 +29,20 @@ function renderPage() {
         } else {
             hourBlock.addClass("future");
         }
-        $(".container").append(hourBlock);
 
 
+        $(".saveBtn").on("click", function (event) {
+            event.preventDefault();
+
+            var storeTime = $(this).attr("saveTime")
+            var x = $(this).attr("data-input");
+            var textInput = "#" + x;
+            var storeText = $(textInput).val();
+
+
+            localStorage.setItem(storeTime, storeText);
+
+        });
     }
 
 }
